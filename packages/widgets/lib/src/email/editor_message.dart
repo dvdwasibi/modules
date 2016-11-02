53ec35d0ffc6d5c22a1326c00ce239ec27763767
David Yang <dayang@google.com>
2016-11-02 14:36:20 -0700

    slow progress
    Change-Id: I3578245d7d3a213d096a75ae48ef44b35b00db32

--- packages/widgets/lib/src/email/editor_message.dart
+++ packages/widgets/lib/src/email/editor_message.dart
@@ -4,3 +4,19 @@
 
 import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
+
+/// Email Message Editor
+/// Users can:
+///   Add/Remove recipients (to, cc, bcc)
+///   Edit Subject line
+///   Edit Message Content
+class EditorMessage extends StatefulWidget {
+
+  /// Subject line of message
+  /// ex. "WFH Today"
+  String subject;
+
+  /// Content of message
+  /// ex. "Need to do laundry and ..."
+  String messageContent;
+}
