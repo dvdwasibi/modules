b75c182d1712cd5f455c096f480e6acdf723a883
David Yang <dayang@google.com>
2016-11-20 22:26:02 -0800

    animations FTW FTW!!!!!!
    Change-Id: I096d3e19b06aa33b5d19ee78df936a3ec52ab1d3

--- packages/widgets/lib/src/shopping/interactive_receipt.dart
+++ packages/widgets/lib/src/shopping/interactive_receipt.dart
@@ -26,6 +26,7 @@ class _InteractiveReceiptState extends State<InteractiveReceipt> with TickerProv
   _InteractiveReceiptState() : super();
 
   AnimationController _controller;
+  Animation<double> _imageAnimation;
 
   bool _editMode = false;
 
@@ -36,7 +37,26 @@ class _InteractiveReceiptState extends State<InteractiveReceipt> with TickerProv
   @override
   void initState() {
     super.initState();
-    _controller = new AnimationController(vsync: this);
+    _controller = new AnimationController(
+      duration: new Duration(milliseconds: 300),
+      vsync: this
+    );
+    _imageAnimation = _initAnimation(Curves.ease, false);
+  }
+
+  Animation<double> _initAnimation(Curve curve, bool inverted) {
+    final CurvedAnimation animation = new CurvedAnimation(
+      parent: _controller,
+      curve: curve
+    );
+
+    return inverted ? new Tween<double>(
+      begin: 2.0,
+      end: 1.0,
+    ).animate(animation) : new Tween<double>(
+      begin: 1.0,
+      end: 2.0,
+    ).animate(animation);
   }
 
   String get price {
@@ -50,11 +70,11 @@ class _InteractiveReceiptState extends State<InteractiveReceipt> with TickerProv
   String get imageUrl {
     switch(_selectedPixelColor) {
       case PixelColor.black:
-        return 'https://www.androidcentral.com/sites/androidcentral.com/files/styles/larger/public/article_images/2016/10/rendered-pixel-black-front-2.png?itok=PcwNz917';
+        return 'https://lh3.googleusercontent.com/WiZ9IdoWExc2vUdR5Oom31lK3BNHeaZ8SRFLgSUl2ObTYineH7LPKLM5NbHaAGXZt0qf';
       case PixelColor.silver:
-        return 'https://www.androidcentral.com/sites/androidcentral.com/files/styles/larger/public/article_images/2016/10/rendered-pixel-silver.png?itok=qQduu9rZ';
+        return 'https://lh3.googleusercontent.com/JKkKltrLkFnpNirTEjb8yA5bui0Hv7mPocx8T5Gu6qUiYrlnt1Jcx7ITH9pobnejSp9u';
       case PixelColor.blue:
-        return 'https://www.androidcentral.com/sites/androidcentral.com/files/styles/larger/public/article_images/2016/10/rendered-pixel-blue.png?itok=ilI-D_u3';
+        return 'https://lh3.googleusercontent.com/7cco-0fPUfmv0D0Rk0dCDYYv1QjzncyGEhxN5zFUHKWoIKuxgvrOwAFbAyRkKxLvv6pV';
     }
   }
 
@@ -88,7 +108,7 @@ class _InteractiveReceiptState extends State<InteractiveReceipt> with TickerProv
       decoration: new BoxDecoration(
         backgroundColor: Colors.grey[800],
       ),
-      // Add Google Store thing Here
+      // TODO(dayang): put in actual Google store LOGO
       child: new Text(
         'Google Store',
         style: new TextStyle(
@@ -100,7 +120,7 @@ class _InteractiveReceiptState extends State<InteractiveReceipt> with TickerProv
     );
   }
 
-  Widget _buildColorSelection(PixelColor pixelColor) {
+  Widget _buildColorOption(PixelColor pixelColor) {
     return new Container(
       margin: const EdgeInsets.symmetric(horizontal: 4.0),
       decoration: new BoxDecoration(
@@ -130,13 +150,49 @@ class _InteractiveReceiptState extends State<InteractiveReceipt> with TickerProv
         return new Flexible(
           flex: 1,
           child: new Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
               new Row(
                 children: <Widget>[
-                  _buildColorSelection(PixelColor.black),
-                  _buildColorSelection(PixelColor.blue),
-                  _buildColorSelection(PixelColor.silver),
+                  _buildColorOption(PixelColor.black),
+                  _buildColorOption(PixelColor.blue),
+                  _buildColorOption(PixelColor.silver),
                 ],
+              ),
+              new Container(
+                margin: const EdgeInsets.only(left: 4.0),
+                child: new DropdownButton<String>(
+                  value: _storageSize,
+                  onChanged: (String newValue) {
+                    setState(() {
+                      if (newValue != null)
+                        _storageSize = newValue;
+                    });
+                  },
+                  items: <String>['32 GB', '128 GB'].map((String value) {
+                    return new DropdownMenuItem<String>(
+                      value: value,
+                      child: new Text(value),
+                    );
+                  }).toList(),
+                ),
+              ),
+              new Container(
+                margin: const EdgeInsets.only(top: 4.0),
+                child: new FlatButton(
+                  child: new Text(
+                    'DONE',
+                    style: new TextStyle(
+                      color: Colors.blue[600],
+                    ),
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      _editMode = false;
+                      _controller.reverse();
+                    });
+                  }
+                ),
               ),
             ],
           ),
@@ -151,25 +207,13 @@ class _InteractiveReceiptState extends State<InteractiveReceipt> with TickerProv
       }
   }
 
-  Widget _buildSizing() {
-
-  }
-
   Widget _buildPixelListing() {
     return new Row(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
-        new AnimatedSize(
-          child: new Image.network(
-            imageUrl,
-            height: _editMode ? 150.0 : 100.0,
-            width: _editMode ? 150.0 : 100.0,
-            fit: ImageFit.contain,
-            gaplessPlayback: true,
-          ),
-          duration: new Duration(milliseconds: 500),
-          curve: Curves.easeInOut,
-          vsync: this,
+        new _AnimatedPixelImage(
+          imageUrl: imageUrl,
+          animation: _imageAnimation,
         ),
         new Flexible(
           flex: 1,
@@ -193,40 +237,10 @@ class _InteractiveReceiptState extends State<InteractiveReceipt> with TickerProv
                   child: new Row(
                     children: <Widget>[
                       _buildPixelData(),
-                      new Text(price),
+                      _editMode ? new Container() : new Text(price),
                     ],
                   ),
                 ),
-                _editMode ? new DropdownButton<String>(
-                  value: _storageSize,
-                  onChanged: (String newValue) {
-                    setState(() {
-                      if (newValue != null)
-                        _storageSize = newValue;
-                    });
-                  },
-                  items: <String>['32 GB', '128 GB'].map((String value) {
-                    return new DropdownMenuItem<String>(
-                      value: value,
-                      child: new Text(value),
-                    );
-                  }).toList(),
-                ) : new Container(),
-                _editMode ? new Container(
-                  child: new FlatButton(
-                    child: new Text(
-                      'DONE',
-                      style: new TextStyle(
-                        color: Colors.blue[600],
-                      ),
-                    ),
-                    onPressed: () {
-                      setState(() {
-                        _editMode = false;
-                      });
-                    }
-                  )
-                ) : new Container(),
               ],
             ),
           ),
@@ -241,6 +255,7 @@ class _InteractiveReceiptState extends State<InteractiveReceipt> with TickerProv
             onPressed: (){
               setState(() {
                 _editMode = !_editMode;
+                _controller.forward();
               });
             },
             icon: new Icon(Icons.edit),
@@ -251,7 +266,22 @@ class _InteractiveReceiptState extends State<InteractiveReceipt> with TickerProv
     );
   }
 
-  Widget _buildPricingTotal() {
+  Widget _buildPricingSection({String label, String value}) {
+    return new Container(
+    padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: new Row(
+        children: <Widget>[
+          new Flexible(
+            flex: 1,
+            child: new Text(label),
+          ),
+          new Text(value),
+        ]
+      ),
+    );
+  }
+
+  Widget _buildPricing() {
     return new Container(
       margin: const EdgeInsets.only(
         left: 64.0,
@@ -270,55 +300,23 @@ class _InteractiveReceiptState extends State<InteractiveReceipt> with TickerProv
       ),
       child: new Column(
         children: <Widget>[
-          new Container(
-          padding: const EdgeInsets.symmetric(vertical: 8.0),
-          child: new Row(
-            children: <Widget>[
-              new Flexible(
-                flex: 1,
-                child: new Text('Subtotal'),
-              ),
-              new Text(price),
-            ]
+          _buildPricingSection(
+            label: 'Subtotal',
+            value: price,
           ),
-        ),
-          new Container(
-          padding: const EdgeInsets.symmetric(vertical: 8.0),
-          child: new Row(
-            children: <Widget>[
-              new Flexible(
-                flex: 1,
-                child: new Text('Shipping & Handling'),
-              ),
-              new Text('\$0.00'),
-            ]
+          _buildPricingSection(
+            label: 'Shipping & Handling',
+            value: '\$0.00',
           ),
-        ),
-          new Container(
-          padding: const EdgeInsets.symmetric(vertical: 8.0),
-          child: new Row(
-            children: <Widget>[
-              new Flexible(
-                flex: 1,
-                child: new Text('Tax'),
-              ),
-              new Text('\$0.00'),
-            ]
+          _buildPricingSection(
+            label: 'Tax',
+            value: '\$0.00',
           ),
-        ),
-          new Container(
-          padding: const EdgeInsets.symmetric(vertical: 8.0),
-          child: new Row(
-            children: <Widget>[
-              new Flexible(
-                flex: 1,
-                child: new Text('Total'),
-              ),
-              new Text(price),
-            ]
+          _buildPricingSection(
+            label: 'Total',
+            value: price,
           ),
-        ),
-        ]
+        ],
       ),
     );
   }
@@ -331,7 +329,6 @@ class _InteractiveReceiptState extends State<InteractiveReceipt> with TickerProv
         left: 48.0,
       ),
       child: new Card(
-        elevation: 2,
         child: new Column(
           mainAxisSize: MainAxisSize.min,
           crossAxisAlignment: CrossAxisAlignment.start,
@@ -361,10 +358,11 @@ class _InteractiveReceiptState extends State<InteractiveReceipt> with TickerProv
             new Container(
               padding: const EdgeInsets.all(16.0),
               child: new Column(
+                mainAxisSize: MainAxisSize.min,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
                   _buildPixelListing(),
-                  _buildPricingTotal(),
+                  _buildPricing(),
                 ]
               ),
             ),
@@ -374,15 +372,142 @@ class _InteractiveReceiptState extends State<InteractiveReceipt> with TickerProv
     );
   }
 
+  Widget _buildUpSellItem({
+    String name,
+    String price,
+    String imageUrl,
+  }) {
+    return new Container(
+      margin: const EdgeInsets.all(16.0),
+      child: new Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          new Container(
+            margin: const EdgeInsets.only(bottom: 4.0),
+            padding: const EdgeInsets.all(15.0),
+            width: 225.0,
+            height: 225.0,
+            decoration: new BoxDecoration(
+              backgroundColor: Colors.grey[300],
+            ),
+            child: new Image.network(
+              imageUrl,
+            ),
+          ),
+          new Text(
+            name,
+            style: new TextStyle(height: 1.5),
+          ),
+          new Text(
+            price,
+            style: new TextStyle(
+              height: 1.5,
+              color: Colors.grey[500],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildUpSell() {
+    return new Container(
+      decoration: new BoxDecoration(
+        backgroundColor: Colors.white,
+      ),
+      child: new Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          new Container(
+            alignment: FractionalOffset.center,
+            padding: const EdgeInsets.only(top: 32.0, bottom: 12.0),
+            child: new Text(
+              'YOU MAY ALSO LIKE',
+              style: new TextStyle(
+                fontSize: 18.0,
+                fontWeight: FontWeight.w400,
+                color: Colors.grey[600],
+              ),
+            ),
+          ),
+          new Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              _buildUpSellItem(
+                name: 'Chromecast Ultra',
+                price: '\$69',
+                imageUrl: 'https://lh3.googleusercontent.com/yq9EWTGMYrpcav3_0ROrNYMA3IC5EJuvpZxNOiEsfMk7dunDdWR2TP_S-Khu1WGejQ',
+              ),
+              _buildUpSellItem(
+                name: 'Google Home',
+                price: '\$129',
+                imageUrl: 'https://lh3.googleusercontent.com/Nu3a6F80WfixUqf_ec_vgXy_c0-0r4VLJRXjVFF_X_CIilEu8B9fT35qyTEj_PEsKw',
+              ),
+            ]
+          ),
+          new Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              _buildUpSellItem(
+                name: 'Artworks Live Case',
+                price: '\$40',
+                imageUrl: 'https://lh3.googleusercontent.com/aYJf9JJIwsNXO_W-1GKBPtdrcfplqzVKUCBUj5sWNJX5jECg3ku68aP0HbgLecEL8A',
+              ),
+              _buildUpSellItem(
+                name: 'Daydrem View',
+                price: '\$79',
+                imageUrl: 'https://lh3.googleusercontent.com/3cTyu0he1Yv6YkDFcsyQURR3H0kQsk0IB8raKeWxtTK_NsngsgnVP5XOLW4cuT9FLME',
+              ),
+            ]
+          ),
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return new Stack(
-      alignment: FractionalOffset.topLeft,
+    return new Block(
       children: <Widget>[
-        _buildBrandHeader(),
-        _buildMainItemCard(),
-      ]
+        new Container(
+          decoration: new BoxDecoration(
+            backgroundColor: Colors.grey[100],
+          ),
+          padding: const EdgeInsets.only(bottom: 32.0),
+          child: new Stack(
+            alignment: FractionalOffset.topLeft,
+            children: <Widget>[
+              _buildBrandHeader(),
+              _buildMainItemCard(),
+            ]
+          ),
+        ),
+        _buildUpSell(),
+      ],
+    );
+
+  }
+}
+
+class _AnimatedPixelImage extends AnimatedWidget{
+
+  final String imageUrl;
+
+  _AnimatedPixelImage({
+    Key key,
+    Animation<double> animation,
+    this.imageUrl,
+  }) : super(key: key, animation: animation);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      height: 100.0 * animation.value,
+      width: 100.0 * animation.value,
+      child: new Image.network(
+        imageUrl,
+        gaplessPlayback: true,
+      ),
     );
   }
 }
